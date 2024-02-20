ecdbcf12cd0e078eade20cc295579e30b410e60f
Matthieu Muller Matthieu.Muller1@grenoble-inp.org <mullemat@bigfoot.u-ga.fr>
2024-02-20 09:17:06 +0100

    Added the training script

---
+++ run_train.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#OAR -n Training
+#OAR -l /nodes=1/gpu=1,walltime=8:00:00
+#OAR --stdout %jobid%.out
+#OAR --stderr %jobid%.err
+#OAR --project pr-fumultispoc
+#OAR -p gpumodel='A100'
+
+cd unrolled_demosaicking
+
+python3 train.py
+
+git add logs/*
+git commit -m "Added new training"
+git push
+
