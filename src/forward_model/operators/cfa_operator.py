67a35ec865da65102ce0cfa1a34787a96d25eb91
Matthieu Muller <matthieu.muller@gipsa-lab.grenoble-inp.fr>
2024-02-26 10:58:43 +0100

    Updated the forward model

--- src/forward_model/operators/cfa_operator.py
+++ src/forward_model/operators/cfa_operator.py
@@ -5,12 +5,7 @@ import numpy as np
 from scipy.sparse import csr_array
 
 from .abstract_operator import abstract_operator
-from .misc.cfa_masks import get_bayer_GRBG_mask, get_bayer_RGGB_mask, get_quad_mask,\
-                            get_sparse_3_mask, get_kodak_mask, get_sony_mask,\
-                            get_chakrabarti_mask, get_honda_mask, get_kaizu_mask,\
-                            get_yamagami_mask, get_gindele_mask, get_hamilton_mask,\
-                            get_wang_mask, get_luo_mask, get_yamanaka_mask,\
-                            get_lukak_mask
+from .misc import cfa_patterns
 
 
 class cfa_operator(abstract_operator):
@@ -24,54 +19,13 @@ class cfa_operator(abstract_operator):
             filters (str): The name of the filters to use for the operation. Default is dirac.
         """
         self.cfa = cfa
+        self.pattern = getattr(cfa_patterns, f'get_{cfa}_pattern')(spectral_stencil, filters)
+        self.pattern_shape = self.pattern.shape
 
-        if self.cfa == 'bayer_GRBG':
-            self.cfa_mask = get_bayer_GRBG_mask(input_shape, spectral_stencil, filters)
+        n = input_shape[0] // self.pattern_shape[0] + (input_shape[0] % self.pattern_shape[0] != 0)
+        m = input_shape[1] // self.pattern_shape[1] + (input_shape[1] % self.pattern_shape[1] != 0)
 
-        if self.cfa == 'bayer_RGGB':
-            self.cfa_mask = get_bayer_RGGB_mask(input_shape, spectral_stencil, filters)
-
-        elif self.cfa == 'quad_bayer':
-            self.cfa_mask = get_quad_mask(input_shape, spectral_stencil, filters)
-
-        elif self.cfa == 'sparse_3':
-            self.cfa_mask = get_sparse_3_mask(input_shape, spectral_stencil, filters)
-
-        elif self.cfa == 'kodak':
-            self.cfa_mask = get_kodak_mask(input_shape, spectral_stencil, filters)
-
-        elif self.cfa == 'sony':
-            self.cfa_mask = get_sony_mask(input_shape, spectral_stencil, filters)
-
-        elif self.cfa == 'chakrabarti':
-            self.cfa_mask = get_chakrabarti_mask(input_shape, spectral_stencil, filters)
-
-        elif self.cfa == 'honda':
-            self.cfa_mask = get_honda_mask(input_shape, spectral_stencil, filters)
-
-        elif self.cfa == 'kaizu':
-            self.cfa_mask = get_kaizu_mask(input_shape, spectral_stencil, filters)
-
-        elif self.cfa == 'yamagami':
-            self.cfa_mask = get_yamagami_mask(input_shape, spectral_stencil, filters)
-
-        elif self.cfa == 'gindele':
-            self.cfa_mask = get_gindele_mask(input_shape, spectral_stencil, filters)
-
-        elif self.cfa == 'wang':
-            self.cfa_mask = get_wang_mask(input_shape, spectral_stencil, filters)
-
-        elif self.cfa == 'luo':
-            self.cfa_mask = get_luo_mask(input_shape, spectral_stencil, filters)
-
-        elif self.cfa == 'yamanaka':
-            self.cfa_mask = get_yamanaka_mask(input_shape, spectral_stencil, filters)
-
-        elif self.cfa == 'lukak':
-            self.cfa_mask = get_lukak_mask(input_shape, spectral_stencil, filters)
-
-        elif self.cfa == 'hamilton':
-            self.cfa_mask = get_hamilton_mask(input_shape, spectral_stencil, filters)
+        self.mask = np.kron(np.ones((n, m, 1)), self.pattern)[..., :input_shape[0], :input_shape[1]]
 
         super().__init__(input_shape, input_shape[:-1])
 
@@ -84,7 +38,7 @@ class cfa_operator(abstract_operator):
         Returns:
             np.ndarray: The output array. Must be of shape self.output_shape.
         """
-        return np.sum(x * self.cfa_mask, axis=2)
+        return np.sum(x * self.mask, axis=2)
 
     def adjoint(self, y: np.ndarray) -> np.ndarray:
         """A method performing the computation of the adjoint of the operator.
@@ -95,7 +49,7 @@ class cfa_operator(abstract_operator):
         Returns:
             np.ndarray: The output array. Must be of shape self.input_shape.
         """
-        return self.cfa_mask * y[..., np.newaxis]
+        return self.mask * y[..., np.newaxis]
 
     @property
     def matrix(self) -> csr_array:
@@ -111,6 +65,6 @@ class cfa_operator(abstract_operator):
         cfa_i = np.repeat(np.arange(N_ij), N_k)
         cfa_j = np.arange(N_ijk)
 
-        cfa_data = self.cfa_mask[cfa_i // self.input_shape[1], cfa_i % self.input_shape[1], cfa_j % N_k]
+        cfa_data = self.mask[cfa_i // self.input_shape[1], cfa_i % self.input_shape[1], cfa_j % N_k]
 
         return csr_array((cfa_data, (cfa_i, cfa_j)), shape=(N_ij, N_ijk))
