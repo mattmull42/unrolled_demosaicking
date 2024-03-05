d3d0ec942d21bee77300774cca8ed95b92742449
Matthieu Muller <matthieu.muller@gipsa-lab.grenoble-inp.fr>
2024-03-05 13:47:25 +0100

    Removed wrong model

--- src/forward_model/operators/__init__.py
+++
@@ -1,6 +0,0 @@
-"""A file allowing to import the different operators.
-"""
-
-from .abstract_operator import abstract_operator
-from .cfa_operator import cfa_operator
-from .binning_operator import binning_operator
