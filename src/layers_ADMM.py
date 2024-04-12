d796f33a1ec0aa96550fc1d3c5ed498c92ce9ee5
Matthieu Muller <matthieu.muller@gipsa-lab.grenoble-inp.fr>
2024-04-12 14:54:27 +0000

    Reduced the number of parameters in the activations

--- src/layers_ADMM.py
+++ src/layers_ADMM.py
@@ -107,10 +107,10 @@ class DoubleConv(nn.Module):
         self.double_conv = nn.Sequential(
             nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(mid_channels),
-            nn.PReLU(mid_channels),
+            nn.PReLU(),
             nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(out_channels),
-            nn.PReLU(out_channels)
+            nn.PReLU()
         )
 
     def forward(self, x):
