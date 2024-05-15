70f34200d951374f9ad85f7af15c51d2368ca683
Matthieu Muller <matthieu.muller1@grenoble-inp.fr>
2024-05-15 16:20:50 +0200

    Better CFAs managing

--- train.py
+++ train.py
@@ -10,22 +10,24 @@ from src.utils import get_dataloader, set_matmul_precision
 set_matmul_precision()
 
 # Declares the hyperparameters
-CFAS = sorted(['bayer_GRBG', 'quad_bayer', 'gindele', 'chakrabarti', 'hamilton', 'sony', 'honda', 'kaizu', 'kodak', 'sparse_3', 'wang', 'yamagami', 'yamanaka'])
+CFAS_TRAIN = sorted(['bayer_GRBG', 'quad_bayer', 'gindele', 'chakrabarti', 'hamilton', 'binning', 'honda', 'kaizu', 'kodak', 'sparse_3', 'wang', 'yamagami', 'yamanaka', 'random'])
+CFAS_TEST = sorted(['luo', 'lukac', 'xtrans', 'honda2', 'bayer_RGGB', 'sony'])
+CFAS = sorted(CFAS_TRAIN + CFAS_TEST)
 CFA_VARIANTS = 2
 TRAIN_DIR = 'input/train'
 VAL_DIR = 'input/val'
 PATCH_SIZE = 64
-NB_STAGES = 4
+NB_STAGES = 6
 NB_CHANNELS = 32
 BATCH_SIZE = 128
 LEARNING_RATE = 1e-2
 MAX_TIME = '00:47:30:00'
 
 # Declares the datasets
-train_dataset = RGBDataset(TRAIN_DIR, CFAS, cfa_variants=CFA_VARIANTS, patch_size=PATCH_SIZE, stride=PATCH_SIZE // 2)
+train_dataset = RGBDataset(TRAIN_DIR, CFAS_TRAIN, cfa_variants=CFA_VARIANTS, patch_size=PATCH_SIZE, stride=PATCH_SIZE // 2)
 train_dataloader = get_dataloader(train_dataset, BATCH_SIZE, shuffle=True)
 
-val_dataset = RGBDataset(VAL_DIR, CFAS, cfa_variants=CFA_VARIANTS)
+val_dataset = RGBDataset(VAL_DIR, CFAS, cfa_variants=0)
 val_dataloader = get_dataloader(val_dataset, BATCH_SIZE)
 
 # Initializes the network and its trainer
